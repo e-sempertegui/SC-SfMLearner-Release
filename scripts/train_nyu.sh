f1ed4293a42022a457a3aba0aff551607811abd7
Emilk <emilksempertegui@yahoo.com>
2021-06-05 15:44:26 +0200

    configured params for photometric loss only

--- scripts/train_nyu.sh
+++ scripts/train_nyu.sh
@@ -4,11 +4,11 @@ python train.py $TRAIN_SET \
 --folder-type pair \
 --resnet-layers 18 \
 --num-scales 1 \
--b16 -s0.1 -c0.5 --epoch-size 0 --epochs 50 \
+-b16 -s0 -c0 --epoch-size 0 --epochs 50 \
 --lr 1e-4 \
 --with-ssim 1 \
---with-mask 1 \
---with-auto-mask 1 \
+--with-mask 0 \
+--with-auto-mask 0 \
 --with-pretrain 1 \
 --log-output --with-gt \
 --dataset nyu \
